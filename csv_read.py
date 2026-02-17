c87be92c4a78e878e4834cb5fab7cfc9d13658a8
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-02-17 20:01:34 +0500

    update message

--- csv_read.py
+++ csv_read.py
@@ -12,3 +12,30 @@ print(csv2)
 
 # colums ko index k through bhi fetch kr skte
 csv3=pd.read_csv("E:\\Pandas\\info.csv",usecols=[0,1])
+print(csv3)
+# header files skip(rows skip)
+
+csv_4=pd.read_csv("E:\\Pandas\\info.csv",skiprows=1)
+print(csv_4)
+
+# index ko change krna
+csv_5=pd.read_csv("E:\\Pandas\\info.csv",index_col="name ")
+print(csv_5)
+
+# heading ko change krna mtln kisi row ki values of heading bnana
+csv_6=pd.read_csv("E:\\Pandas\\info.csv",header=2)
+print(csv_6)
+# headings ka name apni mrzi se likhna
+
+csv_7=pd.read_csv("E:\\Pandas\\info.csv",names=["col1","col2","cl3","col4"])
+print(csv_7)
+# header ko remove krna
+csv_8=pd.read_csv("E:\\Pandas\\info.csv",header=None)
+print(csv_8)
+# prefix add krna
+# csv_9=pd.read_csv("E:\\Pandas\\info.csv",prefix="col")
+# print(csv_9)
+
+# datatype ko change krna
+csv_10=pd.read_csv("E:\\Pandas\\info.csv",dtype={"name ":str,"Grade":str})
+print(csv_10)
