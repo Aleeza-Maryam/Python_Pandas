b5357663f8597589df19af5f1444fa0255cfb8c8
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-02-22 21:40:49 +0500

    update message

--- replace_interpolate.py
+++ replace_interpolate.py
@@ -11,3 +11,22 @@ print(var2)
 # reqex(regular expression)
 var3=var.replace("[A-Za-z]","python",regex=True)
 print(var3)
+
+
+# using dictionary
+var4=var.replace({
+    "name ":'[A-Za-z]'
+},22,regex=True)
+print(var4)
+
+
+
+# backward replace
+print(var)
+var5=var.replace(23,method="bfill")
+print(var5)
+
+
+# ffill
+var6=var.replace("Zoya",method="ffill")
+print(var6)
