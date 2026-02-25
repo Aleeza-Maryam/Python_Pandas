15e0cff8d7f939b319bf63ba631f8246eedf7b91
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-02-25 21:40:39 +0500

    update message

--- concat_merge.py
+++ concat_merge.py
@@ -58,3 +58,42 @@ print(pd.merge(var1,var2,left_index=True,right_index=True,suffixes=("name","pyth
 
 
 
+# concate
+
+# 2 seriees ko jin krna
+sr1=pd.Series([1,2,3,4])
+sr2=pd.Series([5,6,7,8])
+print(pd.concat([sr1,sr2]))
+
+# for dataframes
+
+var7=pd.DataFrame(
+    {
+        "A":[1,2,3,4],
+        "B":[2,3,4,5]
+    }
+)
+var8=pd.DataFrame(
+    {
+        "A":[1,2,3,5],
+        "C":[6,7,8,9]
+    }
+)
+print(pd.concat([var7,var8]))
+print(pd.concat([var7,var8],axis=1))
+
+# NaN ko htana 
+d1=pd.DataFrame(
+    {
+        "A":[1,2,3,4],
+        "B":[2,3,4,5]
+    }
+)
+d2=pd.DataFrame(
+    {
+        "A":[1,2],
+        "C":[6,7]
+    }
+)
+print(pd.concat([d1,d2],axis=1,join="outer"))   #Nan show hoga
+print(pd.concat([d1,d2],axis=1,join="inner"))   #Nan show nhi hoga 
